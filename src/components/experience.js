645a5beed0306dc32b363dad4551bb2f9f2afae5
taylorsieling <taylorasieling@gmail.com>
2021-07-16 21:32:54 -0500

    building experience timeline

---
+++ src/components/experience.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Experience() {
+    return (
+        <section id="experience">
+            <div className="experience">
+                <div className="title">
+                    <h1>Experience</h1>
+                </div>
+                <div class="timeline">
+                    <div class="container left">
+                        <div class="content">
+                        <h2>Jan 2019 - May 2019</h2>
+                        <p>IP Orange</p>
+                        </div>
+                    </div>
+                    <div class="container right">
+                        <div class="content">
+                        <h2>May 2018 - Aug 2018</h2>
+                        <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                    <div class="container left">
+                        <div class="content">
+                        <h2>Aug 2017 - Dec 2017</h2>
+                        <p>Lorem ipsum..</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
