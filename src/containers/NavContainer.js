0dfc1044e6ae48e999ab1fafb2d24e4d4f1394dd
taylorsieling <taylorasieling@gmail.com>
2021-07-18 12:35:55 -0500

    creating root class

---
+++ src/containers/NavContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+export default function NavContainer() {
+    return (
+        <div>
+            <Navigation/>
+        </div>
+    )
+}
